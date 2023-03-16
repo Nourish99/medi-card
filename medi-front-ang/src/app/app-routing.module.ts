9ac5beeb12543c5e4d11d3083737a3204901ba6e
Nourish99 <gear050209@gmail.com>
2023-03-16 15:49:05 -0600

    BULMA

--- medi-front-ang/src/app/app-routing.module.ts
+++ medi-front-ang/src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LandingComponent } from './components/landing/landing.component';
 
-const routes: Routes = [{path: '/', component: LandingComponent}];
+const routes: Routes = [{path: '', component: LandingComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
