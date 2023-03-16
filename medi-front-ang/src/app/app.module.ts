8b32792f46408c09f19736ee986afeb8af134945
Nourish99 <gear050209@gmail.com>
2023-03-16 15:12:31 -0600

    .

--- medi-front-ang/src/app/app.module.ts
+++ medi-front-ang/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingComponent } from './components/landing/landing.component';
+import { AuthComponent } from './components/auth/auth.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { PatientsComponent } from './components/patients/patients.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    AuthComponent,
+    NavbarComponent,
+    FooterComponent,
+    PatientsComponent
   ],
   imports: [
     BrowserModule,
